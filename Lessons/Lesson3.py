db546617f0b627cc2bbed71bcee9a8cb8c0de8c9
Смольянов Николай Александрович <ybrjkfq83@mail.ru>
2023-04-22 13:13:22 +0200

    Add method sum_str

--- Lessons/Lesson3.py
+++ Lessons/Lesson3.py
@@ -1,9 +1,21 @@
-def sum_numbers(n, y = "hello"):
-    print (y)
-    sum=1
-    for i in range (1, n+1):
-        sum+=1
-    return sum
+# def sum_numbers(n, y = "hello"):
+#     print (y)
+#     sum=1
+#     for i in range (1, n+1):
+#         sum+=1
+#     return sum
 
-a=sum_numbers(5,"Привет")
-print (a)
+# a=sum_numbers(5,"Привет")
+# print (a)
+
+
+
+
+
+def sum_str(*args):
+    res=""
+    for i in args:
+        res+=i
+    return res
+
+print (sum_str("пр","и","вет"))
