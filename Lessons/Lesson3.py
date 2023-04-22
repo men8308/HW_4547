6daa9d17865d56947de381465d5a5a6f5e3bdc63
Смольянов Николай Александрович <ybrjkfq83@mail.ru>
2023-04-22 16:19:07 +0200

    Add file Modul.py

--- Lessons/Lesson3.py
+++ Lessons/Lesson3.py
@@ -12,10 +12,27 @@
 
 
 
-def sum_str(*args):
-    res=""
-    for i in args:
-        res+=i
-    return res
+# def sum_str(*args):
+#     res=""
+#     for i in args:
+#         res+=i
+#     return res
 
-print (sum_str("пр","и","вет"))
+# print (sum_str("пр","и","вет"))
+
+
+
+
+
+# import Modul #импортируем файл с нашей фукцией
+# print(Modul.max1(5,8)) #печатаем результат выполнения функции max1, расположенной в файле Modul
+
+# или можно так
+
+# from Modul import max1 # или так from Modul import *
+# print(max1(55,8))
+
+# или можно так
+
+import Modul as M
+print(M.max1(5,8))
